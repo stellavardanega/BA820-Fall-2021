4a2fd4664aa0f5c32a8beb35446232b270ccfca2
Stella <mstellav@bu.edu>
2021-11-01 17:09:05 -0400

    Update 04-PCA.py

--- class-notes/04-PCA.py
+++ class-notes/04-PCA.py
@@ -111,3 +111,48 @@ plt.title("Eigenvalue")
 sns.lineplot(range(1, len(explvar)+1), explvar)
 plt.axhline(1)
 plt.show()
+
+#EXERCISE
+# fit a pca model for the diamonds
+# make sure you think abotu data pre-processing
+# plot out the explained variance of the new PCAs based on your dataset
+
+SQL = "SELECT * from `questrom.datasets.diamonds`"
+PROJECT = "ba820-fall21"
+dia = pd.read_gbq(SQL, PROJECT)
+
+dia.head(3)
+diamonds = dia.select_dtypes("number")
+diamonds.head(3)
+
+scaler = StandardScaler()
+scaler.fit(diamonds)
+dia_scaled = scaler.transform(diamonds)
+
+pca_dia = PCA()
+pcs_dia = pca_dia.fit_transform(dia_scaled)
+
+#plot % variance explained per PC for the diamon dataset
+explvar_dia = pca_dia.explained_variance_ratio_
+plt.title("% of Variance Explained per PC - Diamonds")
+sns.lineplot(range(1, len(explvar_dia)+1), explvar_dia)
+plt.show()
+
+#### judges
+pca.n_components_
+
+COLS = ['PC' + str(i) for i in range(1, len(varexp)+1)]
+COLS
+
+comps = pca.components_
+loadings = pd.DataFrame(comps.T, columns=COLS, index=judges.columns)
+loadings.head()
+
+sns.heatmap(loadings, cmap='vlag')
+plt.show()
+
+pcs[:5, :5]
+
+j = pd.DataFrame(pcs[:, :2], columns=['pc1', 'pc2'], index=judges.index)
+sns.scatterplot(data=j, x='pc1', y='pc2')
+plt.show()
