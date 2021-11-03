c28e6ef5c7273ac86881c9ce494180c1af85cd01
Stella <mstellav@bu.edu>
2021-11-03 14:34:22 -0400

    Update assignment1.py

--- class-notes/assignment1.py
+++ class-notes/assignment1.py
@@ -7,7 +7,6 @@ from sklearn.cluster import KMeans, DBSCAN
 from sklearn.neighbors import NearestNeighbors
 from scipy.cluster.hierarchy import linkage, dendrogram
 from scipy.cluster.hierarchy import fcluster
-from sklearn.decomposition import PCA
 
 from sklearn import metrics 
 from sklearn.datasets import make_blobs
@@ -24,15 +23,13 @@ forums.sample(3)
 forums_num = forums.select_dtypes("number") #selecting only the numerical columns from the dataset
 forums_num.describe().T
 
-scaler = StandardScaler() #standardizing the values in order to get more accurate results
-scaler.fit(forums_num)
-forum_scaled = scaler.transform(forums_num)
-
 #Hierarchical Clustering
-hc1 = linkage(forum_scaled, method="complete")
+hc1 = linkage(forums_num, method="complete")
+hc2 = linkage(forums_num, method="single")
+hc3 = linkage(forums_num, method="average")
 
 plt.figure(figsize=(15,5))
-dendrogram(hc1, labels=forums_num.index)
+dendrogram(list(hc1, hc2, hc3), labels=forums_num.index)
 plt.show()
 
 cluster = fcluster(hc1, 80, criterion='distance')
@@ -82,35 +79,3 @@ labels = db.labels_
 ### Number of clusters in labels, ignoring noise if present.
 n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
 n_noise_ = list(labels).count(-1)
-
-#PCA
-fcor = forums_num.corr()
-sns.heatmap(fcor, cmap='Reds', center=0)
-plt.show()
-
-pca = PCA()
-pcs = pca.fit_transform(forum_scaled)
-type(pcs)
-pcs.shape
-
-varexp = pca.explained_variance_ratio_
-type(varexp)
-varexp.shape
-np.sum(varexp)
-
-#plotting variance explained by PC
-plt.title("Explained Variance per PC")
-sns.lineplot(range(1, len(varexp)+1), varexp)
-plt.show()
-
-#cumulative running percentage
-plt.title("Explained Variance per PC")
-sns.lineplot(range(1, len(varexp)+1), np.cumsum(varexp))
-plt.axhline(.95)
-plt.show()
-
-explvar = pca.explained_variance_
-plt.title("Eigenvalue")
-sns.lineplot(range(1, len(explvar)+1), explvar)
-plt.axhline(1)
-plt.show()
