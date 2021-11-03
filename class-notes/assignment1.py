5158beabd58cd6caa5859b4a1216c15a32d4f159
Stella <mstellav@bu.edu>
2021-11-03 17:28:08 -0400

    Update assignment1.py

--- class-notes/assignment1.py
+++ class-notes/assignment1.py
@@ -19,44 +19,49 @@ forums = pd.read_pickle("~/Documents/GitHub/BA820-Fall-2021/assignments/assignme
 type(forums)
 forums.shape
 forums.sample(3)
+forums.isnull().sum().sum()
 
 forums_num = forums.select_dtypes("number") #selecting only the numerical columns from the dataset
 forums_num.describe().T
 
 #Hierarchical Clustering
-hc1 = linkage(forums_num, method="complete")
-hc2 = linkage(forums_num, method="single")
-hc3 = linkage(forums_num, method="average")
+hc_comp = linkage(forums_num, method="complete")
+hc_sing = linkage(forums_num, method="single")
+hc_avg = linkage(forums_num, method="average")
+hc_ward = linkage(forums_num, method="ward")
 
-plt.figure(figsize=(15,5))
-dendrogram(list(hc1, hc2, hc3), labels=forums_num.index)
+fig, ax = plt.subplots(ncols=4, figsize=(15,5))
+dendrogram(hc_comp, labels=forums_num.index, ax=ax[0])
+dendrogram(hc_sing, labels=forums_num.index, ax=ax[1])
+dendrogram(hc_avg, labels=forums_num.index, ax=ax[2])
+dendrogram(hc_ward, labels=forums_num.index, ax=ax[3])
 plt.show()
 
-cluster = fcluster(hc1, 80, criterion='distance')
+dendrogram(hc_ward, labels=forums_num.index)
+plt.show()
+
+cluster = fcluster(hc_ward, 3, criterion='maxclust')
 forums['cluster'] = cluster
 forums.head()
 
-forums[(forums['cluster']==3)]['text']
+forums[forums['cluster']==1]['text']
 
 #KMeans
 k_range = range(2,15)
 eval = []
+silo_score = []
 
 for i in k_range:
     k = KMeans(i)
     labs = k.fit_predict(forums_num)
     eval.append(k.inertia_)
+    silo_score.append(metrics.silhouette_score(forums_num, k.predict(forums_num)))
 
 sns.lineplot(y=eval, x=k_range)
 plt.show()
 
-k9 = KMeans(9)
-k9.fit(forums_num) 
-labs = k9.predict(forums_num)
-forums['k9'] = labs
-
-silo_overall = metrics.silhouette_score(forums_num, k9.predict(forums_num))
-silo_overall
+sns.lineplot(y=silo_score, x=k_range)
+plt.show()
 
 k4 = KMeans(4)
 k4.fit(forums_num) 
@@ -66,16 +71,6 @@ forums['k4'] = labs
 silo_overall = metrics.silhouette_score(forums_num, k4.predict(forums_num))
 silo_overall
 
-skplt.metrics.plot_silhouette(forum_scaled, labs, figsize=(7,7))
+skplt.metrics.plot_silhouette(forums_num, labs, figsize=(7,7))
 plt.show()
 forums.groupby('k4').mean()
-
-## Compute DBSCAN
-db = DBSCAN(eps=0.5, min_samples=5).fit(forums_num)
-core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
-core_samples_mask[db.core_sample_indices_] = True
-labels = db.labels_
-
-### Number of clusters in labels, ignoring noise if present.
-n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
-n_noise_ = list(labels).count(-1)
